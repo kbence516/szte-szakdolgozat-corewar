7008ea028438b839dd3d9a76ffaec1488eea37e6
Kovács Bence <kbence516@gmail.com>
2025-05-10 19:57:22 +0200

    add: base game mechanics and I/O for cli

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
 ﻿class Program {
+
+    public static void Main(string[] args) {
+        VM vm = VM.GetInstance();
+        for (int i = 0; i < vm.Warriors; i++) {
+            Console.Write($"Kérem {i + 1}. játékos programjának fájlnevét! ");
+            string? path = Console.ReadLine();
+            if (path != null) {
+                Player p = new Player(Path.Combine("../../../", path));
+            }
+        }
+        vm.Play();
+    }
 }
