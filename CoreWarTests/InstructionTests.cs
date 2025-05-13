13ae7208ee90bf7662ce32f0dfefc58f5963e4c3
Kovács Bence <kbence516@gmail.com>
2025-05-14 01:31:38 +0200

    test: Instruction copy

---
+++ CoreWarTests/InstructionTests.cs
@@ -0,0 +1,30 @@
+﻿using CoreWar;
+
+namespace CoreWarTests {
+    [TestClass]
+    public sealed class InstructionTests {
+        [TestInitialize]
+        public void TestInitialize() {
+            VM.ResetInstance();
+        }
+
+        [TestMethod]
+        public void Copy_CopyToEmptyAddress_ValuesChange() {
+            VM vm = VM.GetInstance(memorySize: 1);
+            vm.InstructionAt(0, false).Copy(
+                new Instruction(
+                    OpCode.MOV,
+                    OpModifier.AB,
+                    new Operation(AddressingMode.IMMEDIATE, 1),
+                    new Operation(AddressingMode.IMMEDIATE, 2)
+                )
+            );
+            Assert.AreEqual(new Instruction(
+                OpCode.MOV,
+                OpModifier.AB,
+                new Operation(AddressingMode.IMMEDIATE, 1),
+                new Operation(AddressingMode.IMMEDIATE, 2)
+                ), vm.InstructionAt(0, false));
+        }
+    }
+}
